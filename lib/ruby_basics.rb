327b6fa4ff615c0e265f4399c6d0d12c6d0b1e94
A Glazer <avigayilglazer@gmail.com>
2018-12-24 14:35:33 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -7,21 +7,23 @@ def assign_variable(value)
 end
 
 def argue
-  
+  argue = hello
 end
 
-def greeting
-
+def greeting(name, program)
+  greeting = "My name is #{name} and I am studying #{program}." 
 end
 
 def return_a_value
-
+  return_a_value = "Nice"
+  return_a_value
 end
 
 def last_evaluated_value
-
+  last_evaluated_value = "expert"
+  last_evaluated_value
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  pizza_party = topping
 end
